15118944fa1f717444446c1d37eafa306740ecf1
Duy <phamkhanhduyk49a1@gmail.com>
2025-11-13 00:17:26 +0700

    fix

--- index.php
+++ index.php
@@ -11,7 +11,6 @@
   <button><a href="create.php">+ Thêm sản phẩm</a></button>
   <table border="2" cellpadding="20">
     <tr>
-      <th>ID</th>
       <th>Tên</th>
       <th>Giá</th>
       <th>Mô tả</th>
@@ -24,7 +23,6 @@
     while ($row = $result->fetch_assoc()):
     ?>
       <tr>
-        <td><?= $row['id'] ?></td>
         <td><?= $row['name'] ?></td>
         <td><?= $row['price'] ?> VNĐ</td>
         <td><?= $row['description'] ?></td>
